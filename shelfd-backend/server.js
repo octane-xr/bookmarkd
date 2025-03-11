6bb12a348b496cb140174809ced4c6c8a3596b1a
octane-xr <octirie@gmail.com>
2025-03-11 17:27:14 -0300

    Added proper backend

---
+++ shelfd-backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bookRoutes from './routes/books.js'
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/books', bookRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Backend funcionando');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
